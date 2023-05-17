b5bcc546144ae9ec10a65eb056865d4e16ea65e7
Ulises Martinez <ulimxtz@gmail.com>
2023-05-17 14:30:53 -0600

    Modelo Tipo Cuenta Creado

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -1,6 +1,16 @@
-﻿namespace ProyectoFinanzas_DWI_.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinanzas_DWI_.Models
 {
     public class TipoCuenta
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Nombre { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public int Orden { get; set; }
     }
 }
